d2f58caf4c24a9e32a78fc5334c52240d811e9b2
Mango <sergey.rautbart@gmail.com>
2024-04-13 23:48:46 +0300

    camera zoom

--- Assets/Scripts/StateSystem/StateManager.cs
+++ Assets/Scripts/StateSystem/StateManager.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class StateManager : MonoBehaviour
 {
+    private CameraFollow playerCam;
+
     private void Awake()
     {
         EventManager.ResetManager();
         EventManager.eventOnStateUpdate += CheckIncident;
     }
 
+    private void Start() => playerCam = Camera.main.GetComponent<CameraFollow>();
+
     private void CheckIncident(string incident)
     {
         switch (incident) 
         {
             case "EatFood":
-
+                playerCam.Zoom();
+                Debug.Log(playerCam.ToString());
                 break;
             case "Death":
                               
