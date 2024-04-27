27750380fd1fb578201d81fb6201aae49ab2dfd3
Mango <sergey.rautbart@gmail.com>
2024-04-27 11:46:34 +0300

    try to fix

--- Assets/Scripts/Food.cs
+++ Assets/Scripts/Food.cs
@@ -7,12 +7,13 @@ public class Food : NetworkBehaviour
 {
     private void OnCollisionEnter2D(Collision2D other)
     {
+        Debug.Log(1);
         if (!other.gameObject.CompareTag("Snake")) return;
-
+        Debug.Log(2);
         if (!NetworkManager.Singleton.IsServer) return;
-
+        Debug.Log(3);
         if (other.gameObject.TryGetComponent(out SnakeGrow snakeGrow)) snakeGrow.Grow();
-
+        Debug.Log(4);
         NetworkObject.Despawn(true);
     }
 }
